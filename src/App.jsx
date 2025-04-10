e8dd6d2fce42eb1736e2234c8404502b7c109866
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-04-10 15:41:04 +0200

    feat: first add form

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,35 @@ function App() {
   const [films, setFilms] = useState(filmsArray)
   const [selectedGenre, setSelectedGenre] = useState("")
   const [selectedFilm, setSelectedFilm] = useState("")
+  const [newFilm, setNewFilm] = useState("")
+  const [newFilmGenre, setNewFilmGenre] = useState("")
 
+
+  // USE EFFECT DEL FILTRO 
   useEffect(() => {
     let filteredFilms = filmsArray
     if (selectedGenre !== "") {
       filteredFilms = filmsArray.filter(film => film.genre === selectedGenre)
     }
     if (selectedFilm !== "") {
-      filteredFilms = filteredFilms.filter(film => film.title.includes(selectedFilm))
+      filteredFilms = filteredFilms.filter(film => film.title.toLocaleLowerCase().includes(selectedFilm.toLowerCase()))
     }
     setFilms(filteredFilms)
   }, [selectedGenre, selectedFilm])
+
+  const addNewFilm = (newFilm, newFilmGenre) => {
+    if (newFilm === '' || newFilmGenre === '') {
+      return
+    }
+    const newFilmObject = {
+      title: newFilm,
+      genre: newFilmGenre
+    }
+    const updatedFilms = [...films, newFilmObject]
+    setFilms(updatedFilms)
+    setNewFilm('')
+    setNewFilmGenre('')
+  }
 
 
 
@@ -36,10 +54,8 @@ function App() {
       <div>
         <select onChange={(event) => setSelectedGenre(event.target.value)} value={selectedGenre}>
           <option value="">---</option>
-          <option>Fantascienza</option>
-          <option>Thriller</option>
-          <option>Romantico</option>
-          <option>Azione</option>
+          {filmsArray.map(film =>
+            <option>{film.genre}</option>)}
         </select>
       </div>
       <label>filtra per nome</label>
@@ -55,7 +71,29 @@ function App() {
             <h4>{film.title}</h4>
             <p>{film.genre}</p>
           </li>)}
+        {films.length === 0 && <p>Nessun film trovato 😢</p>}
       </ul>
+      <form onSubmit={(event) => {
+        event.preventDefault()
+        addNewFilm(newFilm, newFilmGenre)
+      }}>
+        <h3>Inserisci un nuovo film nella lista</h3>
+        <label>Inserisci titolo del nuovo film</label>
+        <div>
+          <input
+            type="text"
+            value={newFilm}
+            onChange={(event) => setNewFilm(event.target.value)} />
+        </div>
+        <label>Inserisci genere del nuovo film</label>
+        <div>
+          <input
+            type="text"
+            value={newFilmGenre}
+            onChange={(event) => setNewFilmGenre(event.target.value)} />
+        </div>
+        <button>aggiungi film</button>
+      </form>
     </>
   )
 }
