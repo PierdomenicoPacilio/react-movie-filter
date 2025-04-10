0a12fcff5edab6381498f222b32067e85ad2a16e
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-04-10 13:42:08 +0200

    scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
 function App() {
 
+  const films = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+
+
   return (
     <>
       <h1>React Movie Filter</h1>
+      <label>seleziona il genere</label>
+      <div>
+        <select>
+          <option value="">---</option>
+          <option>Fantascienza</option>
+          <option>Thriller</option>
+          <option>Romantico</option>
+          <option>Azione</option>
+        </select>
+      </div>
+
+      <ul>
+        {films.map(film =>
+          <li>
+            <h4>{film.title}</h4>
+            <p>{film.genre}</p>
+          </li>)}
+      </ul>
     </>
   )
 }
